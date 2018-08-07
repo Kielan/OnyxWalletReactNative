2f9d739969e32600559e957b53614481c7f380d4
Kielan <ki.lemons@gmail.com>
2018-08-07 14:58:51 -0700

    simplified structure in an attempt to isolate router issue

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-require('./js/WalletApp.js')
+'use strict'
+import { Navigation } from 'react-native-navigation'
+//import { Provider } from 'react-redux'
+//import { falcoWalletTheme } from './constants'
+import { registerScreens } from './js/screens'
+//import { configureStore } from './store/configureStore'
+
+const navigatorStyle = {
+	navBarTranslucent: true,
+	drawUnderNavBar: true,
+	navBarTextColor: 'white',
+	navBarButtonColor: 'white',
+	statusBarTextColorScheme: 'light',
+	drawUnderTabBar: true
+};
+
+// Register screens
+registerScreens()
+
+// Start application
+Navigation.events().registerAppLaunchedListener(() => {
+
+
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'walletapp.Login'
+      }
+    },
+  })
+})
